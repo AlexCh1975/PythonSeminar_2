37675609b6913050a479dabcad8a2ae88f9fbce1
AlexCh <stovertikal@mail.ru>
2022-09-28 13:34:27 +0700

    add task 003

---
+++ task_003/2_3.py
@@ -0,0 +1,11 @@
+# Напишите программу, в которой пользователь будет задавать две строки,
+# программа -  определять количество вхождений одной строки в другой.
+
+# in gipopotampo po out 3;
+# in gipopotampo ta out 1;
+# in gipopotampo fa out 0
+
+str1 = "gipopotampo"
+str2 = input("Введите строку: ")
+
+print(str1.count(str2))
